301d4e376f25a5dbd6e602bd1d4f20eeda1a57b2
Indyz <Indyz@MICHAELFLABTOP>
2026-09-18 17:53:04 -0700

    whole code

--- movinf circle/Game.cs
+++ movinf circle/Game.cs
@@ -16,6 +16,9 @@ namespace MohawkGame2D
         public void Setup()
         {
 
+    Window.SetTitle("moving circle test");
+            Window.SetSize(400, 400);
+
         }
 
         /// <summary>
@@ -23,6 +26,25 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            // always make sure Color is capatlized
+            Window.ClearBackground(Color.White);
+
+            Draw.FillColor = Color.Green;
+            Draw.LineSize = 2;
+            Draw.LineColor = Color.Cyan;
+
+            //x y radius(size), if space bar is down can see green circle
+            if (Input.IsKeyboardKeyDown(KeyboardKey.Space))
+            {
+
+                //when mouse left and space bar down at same time change color
+                if (Input.IsMouseButtonDown(MouseButton.Left))
+                {
+                    Draw.FillColor = Color.Red;
+                }
+
+                Draw.Circle(Input.GetMouseX(), Input.GetMouseY(), 50);
+            }
 
         }
     }
